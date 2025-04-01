e33f528adcaef44362bc5ea8b88fe486c37966c1
Mazen Elemam <657889mj@gmail.com>
2025-04-01 11:45:17 +0200

    Removing unused imports in Collision Manager

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -1,8 +1,7 @@
 import java.awt.*;
-import java.awt.image.ImageObserver;
-
 import javax.swing.*;
 
-public class CollisionManager extends Rectangle { 
+public class CollisionManager extends Rectangle {
+    
 }
 
