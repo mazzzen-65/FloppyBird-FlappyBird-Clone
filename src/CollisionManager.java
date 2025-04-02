29052278c20cc5513477551f15109955dc12f091
Mazen Elemam <144605576+mazzzen-65@users.noreply.github.com>
2025-04-02 15:18:34 +0200

    Comment Changes

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 /*
  * this Class is used to handle all of the Collisions 
- * For Pipes , Ground and Bird
+ * For Pipes/Bird
  */
 public class CollisionManager extends Rectangle {
     
