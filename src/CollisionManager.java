3a4075a864079a1bef2a98f28ef452d8f502a649
Mazen Elemam <657889mj@gmail.com>
2025-05-08 01:04:49 +0300

    Start Test on Collisions 2

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -10,32 +10,32 @@ public class CollisionManager {
     public CollisionManager(Pipe pipe, FlappyBird bird) {
         this.birChekBird = bird;
         this.pipchekPipe = pipe;
-
         Timer collisionTimer = new Timer(10, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 checkCollision();
-                if (gameOver) {
-                }
             }
         });
-
+    
         collisionTimer.start();  
     }
 
     public void checkCollision() {
+        System.out.println("check is ");
         Rectangle birdRect = new Rectangle(40, (int) birChekBird._birdY, 50, 50);
 
         for (Pipe pipe : pipchekPipe.pipes) {
             Rectangle pipeRect = new Rectangle(pipe._pipeX + 300, pipe._totY, pipe._pipeWidth, pipe._pipeHeight);
             if (birdRect.intersects(pipeRect)) {
                 gameOver = true;
+                System.out.println("pipe ");
                 return;
             }
         }
 
         if (birChekBird._birdY >= 640) {
             gameOver = true;
+            System.out.println("bird");
         }
     }
 }
