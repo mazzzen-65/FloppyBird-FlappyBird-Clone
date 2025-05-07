1523162fa66555ecb97c5fc2789a89a5e7c13a99
Mazen Elemam <657889mj@gmail.com>
2025-05-07 23:57:52 +0300

    Start Test on Collisions

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -6,5 +6,16 @@ import javax.swing.*;
  */
 public class CollisionManager extends Rectangle {
     
-}
+        Pipe pipchekPipe = new Pipe();
+        FlappyBird birChekBird = new FlappyBird();
+        boolean gameOver = false ;
+    
+        public void checkCollision() {
+            if (birChekBird._birdY == 640) {
+                gameOver = true;
+                System.out.println("stoped");
+            }
+        }
+    }
+
 
